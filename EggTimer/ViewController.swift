206b9bcf029db5f34f7a43a45833cf51069f940a
Michael Dinh <tonkoalsce@gmail.com>
2020-12-06 17:01:52 +1100

    Added egg timer progress

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var eggTitle: UILabel!
     
     let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
     var secondsRemaining: Int = 0;
+    var selectedEggTime = 1;
     var timer = Timer()
     
     // Ranges a...b full inclusive
@@ -27,17 +29,27 @@ class ViewController: UIViewController {
             let buttonTitle : String = sender.currentTitle!
             
             // We are syre since the current title comes from the button
-            secondsRemaining = eggTimes[buttonTitle]!
-            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-                if self.secondsRemaining > 0 {
-                    print("\(self.secondsRemaining) seconds.")
-                    self.secondsRemaining += -1
-                } else {
-                    Timer.invalidate()
-                    self.eggTitle.text = "Done"
-                    
-                }
-            }
+            eggTitle.text = "Cooking..."
+            secondsRemaining = eggTimes[buttonTitle]! * 60
+            selectedEggTime = secondsRemaining
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func setProgress(progress: Int) {
+        progressBar.progress = Float(secondsRemaining) / Float(selectedEggTime)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            self.secondsRemaining += -1
+            self.setProgress(progress: secondsRemaining)
+            
+        } else {
+            eggTitle.text = "Done"
+            setProgress(progress: secondsRemaining)
+            
         }
     }
 }
